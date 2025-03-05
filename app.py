08be5ad4eea31fe6b302459a4d47645b3b0d37de
Saicharan Ritwik Chinni <109512004+SaicharanRitwik39@users.noreply.github.com>
2025-03-05 18:16:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from datetime import datetime
 
 def MCS(uploaded_file, column_name, target_date, num_simulations, bins):
